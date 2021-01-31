f7bc50d7b54beeefaf0abf206475cc8f537e6617
furbo1 <al_razvan@yahoo.com>
2021-01-31 20:15:42 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<UserError> userErrors= new List<UserError>();
+
+            NumericInputError numErr = new NumericInputError();
+            TextInputError txtErr = new TextInputError();
+          
+            userErrors.Add(numErr);
+            userErrors.Add(txtErr);
+
+            foreach (var userErr in userErrors)
+            {
+                Console.WriteLine(userErr.UEMessage(""));
+            }
+           
+        }
+    }
+}
---
+++ UserError.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex3
+{
+    abstract class UserError
+    {
+       
+        public UserError()
+        {
+            //this.UEMessage();
+
+
+        }
+
+        public virtual string UEMessage(string str="Some text")
+        {
+           
+            return str;
+        }
+
+        public virtual string UEMessage()
+        {
+            return "Some text";
+           
+        }
+    }
+
+    class NumericInputError: UserError
+
+    {
+        public string Message = "You tried to use a numeric input in a text only field. This fired an error!";;
+        
+        public void NumericInputErrorr()
+        {
+            //this.UEMessage();
+
+
+        }
+
+
+
+        public override string UEMessage(string str = "You tried to use a numeric input in a text only field. This fired an error!")
+        {
+            return str;
+        }
+
+        public override string UEMessage()
+        {
+            return this.Message;
+
+        }
+    }
+
+      
+   
+
+    class TextInputError : UserError
+    {
+        public string Message = "You tried to use a text input in anumericonly field. This fired an error!";
+        public TextInputError()
+        {
+           return Message;
+           
+        }
+
+        public override string UEMessage(string str = "You tried to use a text input in anumericonly field. This fired an error!")
+        {
+            return str;
+        }
+        public override string UEMessage()
+        {
+            return this.Message;
+
+        }
+
+
+    }
+      
+    }
+
